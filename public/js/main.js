d1a753f6b9c534ac7fef89f68af477cdf839f497
galaxy-comet <oleksandrshkabura811@gmail.com>
2022-06-10 21:47:01 -0400

    feat: Implement function that display to selected the search data receive into OMDbAPI

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+let selected = [];
 const processChange = debounce((str) => getApi(str));
 
 function debounce(func, timeout = 300) {
@@ -37,4 +38,41 @@ function searchDisplay(data, str) {
     selected = [];
     $('#search_result').empty().append(searchResult);
     $('#search_str').empty().append(`Result for <span class="text-sm">"${str}"</span>`);
+}
+// select 
+function select(str) {
+    const displayData = {
+        Poster: str.split('~')[0],
+        Title: str.split('~')[1],
+        Type: str.split('~')[2],
+        Year: str.split('~')[3],
+        imdbID: str.split('~')[4]
+    }
+
+    if (selected.length === 0) {
+        selectDisplay(displayData);
+    } else {
+        const found = selected.find(element => element.imdbID === displayData.imdbID);
+
+        if (!found) {
+            selectDisplay(displayData);
+        }
+    }
+}
+// display selected result
+function selectDisplay(data) {
+    const selectdata = `
+        <li id="dlist-${data.imdbID}" class="flex justify-between border-b-[1px]">\
+            <p>${data.Title}</p>\
+            <button onclick="remove(\'${data.imdbID}\')" class="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-1 px-2 rounded">Remove</button>\
+        </li>
+    `;
+    selected.push(data);
+    $('#cbtn-' + data.imdbID).prop('disabled', true);
+    $('#cbtn-' + data.imdbID).removeClass('bg-blue-500 hover:bg-blue-700').addClass('bg-gray-500 hover:bg-gray-700');
+    $('#selected_result').append(selectdata);
+    $('#selected_count').text(selected.length);
+    if (selected.length === 5) {
+        alert("Selected 5 movies");
+    }
 }
