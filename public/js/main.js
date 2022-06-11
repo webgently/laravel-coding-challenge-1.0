8b5bc32292764af644591a223c722633ebbe4b8d
galaxy-comet <oleksandrshkabura811@gmail.com>
2022-06-10 21:40:48 -0400

    feat: Implement function to display the search data receive into OMDbAPI

--- public/js/main.js
+++ public/js/main.js
@@ -13,9 +13,28 @@ function getApi(str) {
         .then(res => res.json())
         .then(data => {
             if (data.Response === 'True') {
-                console.log("error");
+                searchDisplay(data, str);
             } else {
-                console.log(data);
+                $('#search_result').empty().append('<div>No found</div>');
+                $('#search_str').empty().append('<span>Result for</span>');
+                $('#selected_count').text(0);
+                $('#selected_result').empty();
             }
         });
+}
+// display search result
+function searchDisplay(data, str) {
+    let searchResult = '';
+    data.Search.forEach(element => {
+        const displayData = `${element.Poster}~${element.Title}~${element.Type}~${element.Year}~${element.imdbID}`;
+        searchResult += `
+            <li class="flex justify-between border-b-[1px]">\
+                <p>${element.Title}</p>\
+                <button id="cbtn-${element.imdbID}" onclick="select(\'${displayData}\')" class="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-1 px-2 rounded">Nominate</button>\
+            </li>
+        `;
+    });
+    selected = [];
+    $('#search_result').empty().append(searchResult);
+    $('#search_str').empty().append(`Result for <span class="text-sm">"${str}"</span>`);
 }
